4f276893c7484f512c839d68179edd1941555453
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-06 16:12:25 +0530

    47. Route Parameters

--- Section-4/app.js
+++ Section-4/app.js
@@ -3,8 +3,18 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/api/data", (req, res) => {
+app.get("/api/course", (req, res) => {
   res.send([1, 2, 3, 4, 5]);
+});
+
+//this code for id params
+app.get("/api/course/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+// this code for query string
+app.get("/api/course/name", (req, res) => {
+  res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;

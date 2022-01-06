ccb172c9eee19adbe0df19ea5aa360202de4eaa9
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-06 16:36:40 +0530

    48. Handling HTTP GET Requests

--- Section-4/app.js
+++ Section-4/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+let courses = [
+  {
+    id: 1,
+    name: "course - 1",
+  },
+  {
+    id: 2,
+    name: "course - 2",
+  },
+  {
+    id: 3,
+    name: "course - 3",
+  },
+];
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/api/course", (req, res) => {
@@ -9,12 +24,16 @@ app.get("/api/course", (req, res) => {
 
 //this code for id params
 app.get("/api/course/:id", (req, res) => {
-  res.send(req.params.id);
-});
+  let course = courses.find((item) => {
+    return item.id == parseInt(req.params.id);
+  });
+  console.log(course);
 
-// this code for query string
-app.get("/api/course/name", (req, res) => {
-  res.send(req.query);
+  if (!course) {
+    res.status(404).send("404 - Course not found");
+  } else {
+    res.send(JSON.stringify(course));
+  }
 });
 
 const port = process.env.PORT || 3000;

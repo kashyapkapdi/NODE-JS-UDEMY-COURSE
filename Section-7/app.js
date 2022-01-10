486f184b7e84e36736e85530525cfde8fd6782f8
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-10 17:18:36 +0530

    87 Models(learn how to create documents in mongodb using node & express js)

--- Section-7/app.js
+++ Section-7/app.js
@@ -24,3 +24,18 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
+
+const Course = mongoose.model("course", courseSchema);
+
+const createCourse = async () => {
+  const course = new Course({
+    name: "React js Course",
+    author: "Kashyap Kapdi",
+    tags: ["React js", "Frontend"],
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log(result);
+};
+
+createCourse();
